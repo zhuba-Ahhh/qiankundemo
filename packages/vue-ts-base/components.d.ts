66b22f641cb7d59309f605f97701e3f100c936cb
zhuba-Ahhh <3477826311@qq.com>
2023-09-18 17:58:23 +0800

    feat: add ts-base

--- packages/vue-ts-base/components.d.ts
+++ packages/vue-ts-base/components.d.ts
@@ -7,6 +7,8 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ElTabPane: typeof import('element-plus/es')['ElTabPane']
+    ElTabs: typeof import('element-plus/es')['ElTabs']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- packages/vue-tsx/src/App.tsx
+++ packages/vue-tsx/src/App.tsx
@@ -1,14 +1,14 @@
 import { defineComponent } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import './styles/less/App.less'
+import styles from './styles/less/App.module.less'
 
 export default defineComponent({
   name: 'App',
   setup() {
     return () => (
-      <div class="main">
-        <nav>
-          <RouterLink to="/todo">Todo</RouterLink>
+      <div class={styles?.main}>
+        <nav class={styles?.nav}>
+          <RouterLink to="/todo" class={styles}>Todo</RouterLink>
         </nav>
         <RouterView />
       </div>
