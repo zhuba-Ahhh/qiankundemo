17514768e13565ea1b92d5ada11684856d59efdd
zhuba-Ahhh <84793349+zhuba-Ahhh@users.noreply.github.com>
2023-10-25 13:37:38 +0000

    Deploying to gh-pages from @ zhuba-Ahhh/qiankundemo@ca9eb8eb3a8e377c06ab1fc189f9a91dfd86d668 🚀

---
+++ vue-ts/js/about.49596a5e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_ts_"]=self["webpackChunkvue_ts_"]||[]).push([[443],{209:function(n,u,t){t.r(u),t.d(u,{default:function(){return l}});var e=t(236);const s={class:"about"},a=(0,e._)("h1",null,"This is an about page",-1),r=[a];function c(n,u){return(0,e.wg)(),(0,e.iD)("div",s,r)}var i=t(238);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.49596a5e.js.map
